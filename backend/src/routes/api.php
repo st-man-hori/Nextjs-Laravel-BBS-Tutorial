76425668747bbc126abaf4effdd97fbc2c344db3
st-man-hori <77187521+st-man-hori@users.noreply.github.com>
2023-05-22 05:11:16 +0000

    [add]thread api

--- backend/src/routes/api.php
+++ backend/src/routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ThreadController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/threads', [ThreadController::class, 'index']);
+Route::post('/threads', [ThreadController::class, 'store']);
+Route::get('/threads/{id}', [ThreadController::class, 'show']);
